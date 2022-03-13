03c91e6eceacd62c9e30daba81e41149dea5d7fd
James Bradbury <github@jamesbradbury.net>
2022-03-13 17:57:41 +0100

    init

---
+++ src/routes/tickets.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { goto } from '$app/navigation'
+    import { onMount } from 'svelte';
+
+    onMount(async() => {
+        goto('https://wentworthwoodhouse.digitickets.co.uk/tickets')
+    })
+</script>
